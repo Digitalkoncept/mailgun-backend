1e96bc81957ce9d4647bfde48b197edd2a649a2e
digitalkoncept <digitalkoncept@outlook.com>
2022-10-13 16:16:27 +0530

    update index

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const API_KEY = 'key-8b39403f4756fc2a39bc35280ebbded0';
-const DOMAIN = 'ultimatesms.in';
+const api_key = 'key-8b39403f4756fc2a39bc35280ebbded0';
+const domain = 'ultimatesms.in';
 
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-
-const mailgun = new Mailgun(formData);
-const client = mailgun.client({username: 'api', key: API_KEY});
-
-const messageData = {
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+ 
+var data = {
   from: 'Excited User <me@samples.mailgun.org>',
   to: 'sachin.tomar@techcorona.com',
   subject: 'Hello',
   text: 'Testing some Mailgun awesomeness!'
 };
-
-client.messages.create(DOMAIN, messageData)
- .then((res) => {
-   console.log(res);
- })
- .catch((err) => {
-   console.error(err);
- });
+ 
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});
